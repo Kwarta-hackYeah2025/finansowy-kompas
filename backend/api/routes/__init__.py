e01655d6a410e2642af7181ba4b7f8fb1b259035
kkaarroollm <mkarol.4514@gmail.com>
2025-10-04 15:52:55 +0200

    new

--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from backend.api.routes.health import router as health_router
+from .health import router as health_router
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(health_router)
