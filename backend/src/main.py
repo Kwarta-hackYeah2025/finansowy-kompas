2d8e080e6b0ed83308cae12c61c175a163c3676b
kkaarroollm <mkarol.4514@gmail.com>
2025-10-04 13:25:50 +0200

    backend init

---
+++ backend/src/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .config import settings
+from .routes import router
+
+
+app: FastAPI = FastAPI(
+    title=settings.title,
+    version=settings.version,
+    contact={
+        "name": settings.contact_name,
+        "email": settings.contact_email,
+    },
+    openapi_url=None if not settings.environment.docs_available() else "/openapi.json",
+)
+
+app.include_router(router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_allow_origins,
+    allow_credentials=settings.cors_allow_credentials,
+    allow_methods=settings.cors_allow_methods,
+    allow_headers=settings.cors_allow_headers,
+)
