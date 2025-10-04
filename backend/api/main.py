425ad1d3c6a4222646f5c01d45d8dd328f670fdb
Andrzej Legutko <andrzejlegutko@gmail.com>
2025-10-04 16:27:43 +0200

    add port and host to api

--- backend/api/main.py
+++ backend/api/main.py
@@ -4,7 +4,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from ..config import settings
 from .routes import router
 
-
 app: FastAPI = FastAPI(
     title=settings.title,
     version=settings.version,
@@ -25,3 +24,8 @@ app.add_middleware(
     allow_methods=settings.cors_allow_methods,
     allow_headers=settings.cors_allow_headers,
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
